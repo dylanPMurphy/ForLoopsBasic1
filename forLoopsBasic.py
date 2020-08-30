264e5debec23b5d202dfc645b5dcc8d77e46f6b5
dylanPMurphy <dylanmurphydba@gmail.com>
2020-08-29 20:40:16 -0700

    Coding the Dojo Way

--- forLoopsBasic.py
+++ forLoopsBasic.py
@@ -15,6 +15,18 @@ for i in range(5, 1001):
 
 
 #    Counting, the Dojo Way - Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
+
+
+for i in range(0, 151):
+    
+    if i % 5 == 0:
+        print("Coding")
+        if i % 10 == 0:
+            print("Dojo")
+    else:
+        print(i)
+
+
 #    Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
 #    Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
 #    Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
