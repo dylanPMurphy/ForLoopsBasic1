051723a219ee900a61d2d01199cb9df328dc30a4
dylanPMurphy <dylanmurphydba@gmail.com>
2020-08-29 20:45:27 -0700

    countdown by 4s

--- forLoopsBasic.py
+++ forLoopsBasic.py
@@ -29,11 +29,15 @@ for i in range(0, 151):
 
 #    Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
 sum = 0
-for i in range(0, 500000):
-    if i %2 ==1:
+for i in range(1, 500000, 2):
+    
         sum+=i
 
 print(sum)
 
 #    Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
+
+for i in range(2018, 0, -4):
+    print(i)
+
 #    Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
